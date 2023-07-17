218638305ed914f513b6469a3b6519c57c146c4e
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-17 17:08:33 -0300

    override ToString

---
+++ MeusExemplos/POO_Simples/MinhaClasse.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace POO_Simples
+{
+    public class MinhaClasse
+    {
+        //prop + TAB
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return 
+                $"O nome é {Name}.\n" +
+                $"A idade é {Age}.";
+        }
+    }
+}
---
+++ MeusExemplos/POO_Simples/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace POO_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaClasse minhaClasse = new MinhaClasse();
+            minhaClasse.Name = "João";
+            minhaClasse.Age = 10;
+            Console.WriteLine(
+                $"Nome: {minhaClasse.Name}" +
+                $", Idade: {minhaClasse.Age}");
+
+            Console.WriteLine("\n\nOverride do ToString()");
+            Console.WriteLine(minhaClasse);
+        }
+    }
+}
