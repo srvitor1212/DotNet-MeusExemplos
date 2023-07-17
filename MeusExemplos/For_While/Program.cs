043e19d5a1e13a236ef9526ecc6d0b4895da99c0
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-17 16:44:05 -0300

    For While

---
+++ MeusExemplos/For_While/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("...running");
+
+
+Console.WriteLine("For Simples");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+Console.WriteLine("\n\nWhile Simples");
+bool executando = true;
+int contador = 0;
+while (executando)
+{
+    Console.WriteLine($"Contador: {contador}");
+    if (contador == 10)
+        executando= false;
+    else 
+        contador++;
+}
