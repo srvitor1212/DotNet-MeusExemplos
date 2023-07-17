f40a358b5b5b584203316213b19a3e960a32f792
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-17 15:50:54 -0300

    Criação da solution

---
+++ MeusExemplos/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+// cw + TAB
+Console.WriteLine("Hello, World!");
+
+
+int valor = 10;
+int valor2 = 5;
+Console.WriteLine($"Valor do número é {valor}");
+Console.WriteLine("Valor: " + valor);
+Console.Write("Assim soma o valor:"); Console.WriteLine(valor + valor2);
+
+
+Console.WriteLine(
+    "Olá\n" +
+    "Essa mensagem foi formatada\n\n" +
+    "Pulando linhas.");
