f6c352c58cb4dd6997cd1f7d06521032f0a489d8
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-21 12:06:44 -0300

    poo encapsulamento

---
+++ MeusExemplos/POO_Encapsulamento/Produto.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace POO_Encapsulamento
+{
+    public class Produto
+    {
+        public Produto(string name, double preco, int quantidade)
+        {
+            Name = name;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //public
+        public string Name { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        //private
+        private string PropriedadePrivada { get; set; } = "Não Informado";
+
+        public string getPropriedadePrivada()
+        {
+            return PropriedadePrivada;
+        }
+
+        public void setPropriedadePrivada(string propriedadePrivada)
+        {
+            if (propriedadePrivada.Length >= 3)
+                this.PropriedadePrivada = propriedadePrivada;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"name: {Name}.\n" +
+                $"preço: {Preco}.\n" +
+                $"qtd: {Quantidade}.\n" +
+                $"private: {PropriedadePrivada}.\n";
+        }
+    }
+}
---
+++ MeusExemplos/POO_Encapsulamento/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace POO_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...iniciando");
+
+            Produto produto = new Produto("TV", 1200.99, 3);
+            Console.WriteLine(produto);
+            //Posso alterar as propriedades
+            produto.Name = "XPTO";
+            produto.Preco = 1;
+            produto.Quantidade = 1;
+            //produto.PropriedadePrivada = "texto"; //não pode
+            Console.WriteLine(produto);
+
+            
+            Console.WriteLine("...usando set");
+            produto.setPropriedadePrivada("ab");
+            Console.WriteLine(produto);
+            produto.setPropriedadePrivada("Teste");
+            Console.WriteLine(produto);
+
+        }
+    }
+}
