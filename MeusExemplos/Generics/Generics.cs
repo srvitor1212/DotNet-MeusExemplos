abafdf57dd168e89f157ce16f2446c284991133e
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-26 13:40:50 -0300

    Generics com vários tipos

--- MeusExemplos/Generics/Generics.cs
+++ MeusExemplos/Generics/Generics.cs
@@ -10,5 +10,12 @@ namespace Generics
             Console.WriteLine($"tipos: {v1.GetType()} {v2.GetType()}");
             Console.WriteLine($"{v1} é igual a {v2} ? {res}");
         }
+
+        public void CompararTipos<TipoUm, TipoDois>(TipoUm v1, TipoDois v2)
+        {
+            bool res = v1.Equals(v2);
+            Console.WriteLine($"tipos: {v1.GetType()} {v2.GetType()}");
+            Console.WriteLine($"{v1} é igual a {v2} ? {res}");
+        }
     }
 }
--- MeusExemplos/Generics/Program.cs
+++ MeusExemplos/Generics/Program.cs
@@ -11,19 +11,26 @@ namespace Generics
             int i1 = 10, i2 = 10;
             decimal d1 = 10, d2 = 10;
             string s1 = "10", s2 = "10";
-
             // Para cada tipo de valor preciso de uma função nova na classe
             naoGeneric.Comparar(i1, i2);
             naoGeneric.Comparar(d1, d2);
             naoGeneric.Comparar(s1, s2);
 
 
-            // Exempl com Generics
+            // Exemplo com Generics
             Console.WriteLine("...generics");
             var generic = new Generics();
             generic.Comparar(i1, i2);
             generic.Comparar(d1, d2);
             generic.Comparar(s1, s2);
+
+
+            // Generics com 2 ou mais tipos
+            Console.WriteLine("...dois ou mais tipos");
+            var gen2 = new Generics();
+            gen2.CompararTipos(i1, s1);
+            gen2.CompararTipos(d1, d2);
+
         }
     }
 }
