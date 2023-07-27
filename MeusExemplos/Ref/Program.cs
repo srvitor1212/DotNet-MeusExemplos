540bc4ee9e20b506dac0bbc2d2a0a87510493214
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-27 14:25:44 -0300

    ref

---
+++ MeusExemplos/Ref/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace MeuNameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ref pode criar referência mesmo em tipos struct
+            int valor = 10;
+
+            Console.WriteLine("...sem ref");
+            SemRef(valor);
+            Console.WriteLine(valor);
+
+            Console.WriteLine("...usando ref");
+            UsandoRef(ref valor);
+            Console.WriteLine(valor);
+        }
+
+        static void SemRef(int v) { 
+            v = v + 5;
+        }
+
+        static void UsandoRef(ref int v)
+        {
+            v = v + 6;
+        }
+
+    }
+}
