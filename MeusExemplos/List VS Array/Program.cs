c82ec3d89dfbd0e6b25d0fb1c131b1e8bd78b107
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-27 17:26:03 -0300

    Experimento em andamento

---
+++ MeusExemplos/List VS Array/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace MeuNameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int QTD_INDICES = 10000;
+            string TEXTO = "Uma string qualquer para testes";
+
+            string[] meuArray;
+            List<string> meuList;
+
+            // Preparar o ARRAY
+            meuArray = new string[QTD_INDICES];
+            for (int i = 0; i < QTD_INDICES; i++)
+                meuArray[i] = TEXTO;
+
+            // Preparar o LIST
+            meuList = new List<string>();
+            for (int i = 0;i < QTD_INDICES;i++)
+            {
+                meuList.Add(TEXTO);
+            }
+
+            //todo: testar performance de um e de outro
+        }
+    }
+}
