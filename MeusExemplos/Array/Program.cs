20defea47001315c167c404054b03597a430b67e
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-27 13:56:34 -0300

    Pssar Array por parâmetro

--- MeusExemplos/Array/Program.cs
+++ MeusExemplos/Array/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace Array
 {
     class Program
@@ -19,6 +21,7 @@ namespace Array
 
 
             // Alocando classes no vetor
+            // Ocupa muita memória
             MinhaClasse[] classes = new MinhaClasse[100];
 
             for (int i = 0; i < classes.Length; i++)
@@ -27,6 +30,27 @@ namespace Array
                 classes[i].NomeProduto = "Nome do produto";
                 classes[i].Preco = random.Next(0,100);
             }
+
+
+            // Passando um array por parâmetro
+            Console.WriteLine($"Resultado da soma: " +
+                $"{Calcular(new int[]{ 1, 3, 5 })}");
+
+            Console.WriteLine($"Resultado da soma: " +
+                $"{Calcular(new int[] { 231, 322, 52, 323, 242, 42414})}");
+
+        }
+
+
+        static int Calcular(int[] valores)
+        {
+            int res = 0;
+            for (int i = 0; i < valores.Length; i++)
+            {
+                res += valores[i];
+            }
+
+            return res;
         }
     }
 }
