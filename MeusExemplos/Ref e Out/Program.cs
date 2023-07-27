eccdfc572b0fd63ff4f8df9bf2a6eb9c5851a00a
Vitor Vicente <vitor.v@ambevtech.com.br>
2023-07-27 14:31:13 -0300

    Ref e Out

--- MeusExemplos/Ref e Out/Program.cs
+++ MeusExemplos/Ref e Out/Program.cs
@@ -16,15 +16,27 @@ namespace MeuNameSpace
             Console.WriteLine("...usando ref");
             UsandoRef(ref valor);
             Console.WriteLine(valor);
+
+            int val;
+            //UsandoRef(ref val); // O ref exige que a variável seja iniciada;
+            // Out permite passa variável não inicializada
+            UsandoOut(out val);
+            Console.WriteLine($"out: {val}");
         }
 
-        static void SemRef(int v) { 
+        static void SemRef(int v)
+        {
             v = v + 5;
         }
 
         static void UsandoRef(ref int v)
         {
             v = v + 6;
+        }
+
+        static void UsandoOut(out int v)
+        {
+            v = 200;
         }
 
     }
