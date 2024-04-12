fc1c2b0535b56536723b16c30f0824992f4d45b1
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-04-12 09:33:30 -0300

    Removendo

--- MeusExemplos/MinimalApi/Endpoints.cs
+++ MeusExemplos/MinimalApi/Endpoints.cs
@@ -12,11 +12,6 @@ public static class Endpoints
         app.MapGet("/todoitems", async (ContextDb db) =>
             await db.Todos.ToListAsync());
 
-        app.MapGet("/todoitems-filtered", async (ContextDb db) =>
-        {
-            await db.Todos.ToListAsync();
-        });
-
 
 
 
