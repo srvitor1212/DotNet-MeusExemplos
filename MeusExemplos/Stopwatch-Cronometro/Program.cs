9528ed49509895c5b932a72b57678b3ee5a5edb4
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-05-10 14:21:39 -0300

    Stopwatch Cronometro

---
+++ MeusExemplos/Stopwatch-Cronometro/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using System.Diagnostics;
+
+var cronometro = new Stopwatch();
+
+
+//uso
+cronometro.Start();
+Thread.Sleep(1000);
+cronometro.Stop();
+
+//mostrar o tempo decorrido
+Console.WriteLine(cronometro);
+Console.WriteLine(cronometro.Elapsed);
+
+//formatar o tempo decorrido
+var ts = cronometro.Elapsed;
+string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10);
+Console.WriteLine("RunTime " + elapsedTime);
