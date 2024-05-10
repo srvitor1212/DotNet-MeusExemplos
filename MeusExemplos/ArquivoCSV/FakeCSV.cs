c6ba72699679d87bd48cd8504f761e36d55d7a95
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-05-10 14:50:12 -0300

    CSV

---
+++ MeusExemplos/ArquivoCSV/FakeCSV.cs
@@ -0,0 +1,22 @@
+﻿namespace ArquivoCSV
+{
+    internal class FakeCSV
+    {
+        public void GerarCSV(int linhas)
+        {
+            var nomeArquivo = "meu_arquivo.csv";
+            var rand = new Random();
+
+            using (var writer = new StreamWriter(nomeArquivo))
+            {
+                writer.WriteLine("id;nome;valor");
+                for (int i = 0; i < linhas; i++)
+                {
+                    writer.WriteLine($"{i};Nome do registro;{rand.NextDouble()}");
+                }
+            }
+
+            Console.WriteLine("Arquivo gerado: {0}\\{1}", Directory.GetCurrentDirectory(), nomeArquivo);
+        }
+    }
+}
---
+++ MeusExemplos/ArquivoCSV/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using ArquivoCSV;
+
+
+var csv = new FakeCSV();
+csv.GerarCSV(10);
+
+//todo: formas de ler esse CSV
+//https://www.youtube.com/watch?v=SdXIGCU8xuE&ab_channel=EximiaCo-Excel%C3%AAnciaTecnol%C3%B3gica
