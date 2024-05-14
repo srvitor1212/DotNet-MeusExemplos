b6996d42487cc31a58170cd898f857f712350144
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-05-14 10:55:31 -0300

    DateTime exemplo converter de strings

---
+++ MeusExemplos/DateTime/Date Time.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace DateTime_Ex
+{
+    internal class Date_Time
+    {
+        public DateTime StringToDatetime(string value)
+        {
+
+
+            //trata se for null ou spaces
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine(">>é nula ou espaços");
+                return DateTime.MinValue;
+            }
+
+
+            //tenta converter a string para datetime
+            CultureInfo cultureInfo = new CultureInfo("pt-BR");
+            if (DateTime.TryParse(value, cultureInfo, out var result))
+            {
+                Console.WriteLine(">>não foi possível converter");
+                return result;
+            }
+
+            Console.WriteLine(">>retorno default");
+            return DateTime.MinValue;
+        }
+    }
+}
---
+++ MeusExemplos/DateTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using DateTime_Ex;
+
+
+Console.WriteLine("==== Tratando datas null e zeradas/inválidas (ex.: 00/00/0000 00:00)");
+string str1 = "00/00/0000 00:00";
+string? str2 = null;
+string str3 = "";
+
+var convert = new Date_Time();
+Console.WriteLine("str1 = " + convert.StringToDatetime(str1) + "\n");
+Console.WriteLine("str2 = " + convert.StringToDatetime(str2) + "\n");
+Console.WriteLine("str3 = " + convert.StringToDatetime(str3) + "\n");
