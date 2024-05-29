0bcd62f2693478e9a29033c5527b255e9462ed53
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-05-29 08:59:32 -0300

    Ternario

---
+++ MeusExemplos/Ternario/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+// (condição) ? valor_se_verdadeiro : valor_se_false
+string res = (1 > 2) ? "SIM" : "NÃO";
+System.Console.WriteLine(res);
+
+// outro exemplo
+double preco = 100;
+double desconto = (preco > 200) ? preco * 0.10 : preco * 0.15;
+System.Console.WriteLine("Preço R$ " + preco + " desconto R$ " + desconto);
+
+preco = 1000;
+desconto = (preco > 200) ? preco * 0.10 : preco * 0.15;
+System.Console.WriteLine("Preço R$ " + preco + " desconto R$ " + desconto);
+
+
+Console.WriteLine("===== usando função static");
+var valor = IsValid(4) ? $"é menor que 10" : "não válido";
+Console.WriteLine($"valor: {valor}");
+
+static bool IsValid(int valor)
+{
+    if (valor < 10) 
+        return true;
+    
+    return false;
+}
