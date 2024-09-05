03e17936775d523067381c468e9f98f18ebf7985
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-09-05 17:19:48 -0300

    modificadores

---
+++ MeusExemplos/SOLID/a1_Modificadores_de_Acesso.cs
@@ -0,0 +1,13 @@
+﻿namespace SOLID;
+
+public class a1_Modificadores_de_Acesso
+{
+    /*
+    public:                 acesso não é restrito.
+    protected:              acesso é limitado à classe que os contém ou aos tipos derivados da classe que os contém.
+    internal:               acesso é limitado ao assembly atual.
+    protected internal:     acesso é limitado ao assembly atual ou aos tipos derivados da classe que os contém.
+    private:                acesso é limitado ao tipo recipiente.
+    private protected:      acesso é limitado à classe que o contém ou a tipos derivados da classe que o contém no assembly atual.
+     */
+}
