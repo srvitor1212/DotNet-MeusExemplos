47483568f13110b39bbc3f6eb6f42002202f8765
Vitor Vicente <vitor.email2011@gmail.com>
2024-09-06 00:02:51 -0300

    Entidade base

---
+++ Clean_Architecture/Exemplo_Clean_Architecture/Domain/EntityBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; protected set; }
+
+    public DateTimeOffset DataCriacao { get; protected set; }
+
+    public DateTimeOffset DataAtualizacao { get; protected set; }
+
+
+
+
+    protected EntityBase()
+    {
+        Id = Guid.NewGuid();
+        SetDataCriacao();
+    }
+
+    protected EntityBase(Guid id)
+    {
+        Id = id;
+        SetDataCriacao();
+    }
+
+
+
+    public void SetDataAtualizacao() => DataAtualizacao = DateTimeOffset.Now;
+    private void SetDataCriacao() => DataCriacao = DateTimeOffset.Now;
+}
