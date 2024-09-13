4caea43a18c71c0f8fea93e8f57cdd6aa9039f40
Vitor Vicente <vitor.email2011@gmail.com>
2024-09-12 22:47:28 -0300

    Injecao De Dependencia

---
+++ MeusExemplos/SOLID/InjecaoDeDependencia.cs
@@ -0,0 +1,31 @@
+﻿namespace SOLID;
+
+public class InjecaoDeDependencia
+{
+    private readonly DependenciaASerInjetada _dependenciaASerInjetada;
+
+    public InjecaoDeDependencia(DependenciaASerInjetada dependenciaASerInjetada)
+    {
+        _dependenciaASerInjetada = dependenciaASerInjetada;
+    }
+
+    public bool VerificaValor(int valor)
+    {
+        var calculado = _dependenciaASerInjetada.Calculo();
+
+        return valor > calculado;
+    }
+}
+
+
+
+
+public class DependenciaASerInjetada()
+{
+    public int Calculo()
+    {
+        Random rnd = new Random();
+
+        return rnd.Next(15);
+    }
+}
--- MeusExemplos/SOLID/Program.cs
+++ MeusExemplos/SOLID/Program.cs
@@ -1,7 +1,11 @@
-﻿
-
-using SOLID;
+﻿using SOLID;
 
 Console.WriteLine("Hello World!");
 
 new a1_Modificadores();
+
+
+Console.WriteLine("=== InjecaoDeDependencia");
+var obj = new InjecaoDeDependencia(new DependenciaASerInjetada());
+
+Console.WriteLine(obj.VerificaValor(10));
