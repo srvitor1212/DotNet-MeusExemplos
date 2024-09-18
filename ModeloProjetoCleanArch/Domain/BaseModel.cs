c9e0024be0fcdf02b86fb619d8c83dcfb1dfe724
Vitor Vicente <vitor.v@ambevtech.com.br>
2024-09-18 09:12:25 -0300

    BaseModel

---
+++ ModeloProjetoCleanArch/Domain/BaseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain;
+
+public abstract class BaseModel
+{
+    public Guid Id { get; protected set; }
+    public DateTimeOffset DataCriacao { get; protected set; }
+    public DateTimeOffset? DataAtualizacao { get; protected set; }
+
+    protected BaseModel()
+    {
+        SetDataCriacao();
+    }
+
+    protected BaseModel(Guid id)
+    {
+        Id = id;
+        SetDataCriacao();
+    }
+
+    public void SetDataAtualizacao() => DataAtualizacao = DateTimeOffset.Now;
+    public void SetDataAtualizacao(DateTimeOffset date) => date = DateTimeOffset.Now;
+
+    internal void SetDataCriacao() => DataCriacao = DateTimeOffset.Now;
+}
