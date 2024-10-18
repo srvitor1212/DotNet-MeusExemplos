bae5e5b1901577a37b5e29444482fa742e35a919
Vitor Vicente <vitor.email2011@gmail.com>
2024-10-18 15:47:26 -0300

    Calcular total

--- MeusExemplos/CalculoHoras/Program.cs
+++ MeusExemplos/CalculoHoras/Program.cs
@@ -9,10 +9,33 @@ var marcacoes = importar.GetMarcacoes();
 
 var cargaHorariaSemanal = new DateTime(1, 1, 1, 8, 30, 0);
 var cargaHorariaSabado = new DateTime(1, 1, 1, 4, 0, 0);
+var toleranciaExtra = new TimeSpan(0, 5, 59);
+var toleranciaFalta = new TimeSpan(0, -5, -59);
 
 var calculo = new CalculoDeHoras(marcacoes, cargaHorariaSemanal, cargaHorariaSabado);
 
 var resultado = calculo.Calcular();
+
+
+foreach (var item in resultado)
+{
+    Console.WriteLine($"\n\n" +
+        $"--- {item.Nome} ---");
+
+    TimeSpan saldoTotal = TimeSpan.Zero;
+
+    foreach (var dia in item.SaldoPorDias)
+    {
+        Console.WriteLine($"{dia.Data} {dia.HoraTrabalhada} {dia.Saldo}");
+
+        if (dia.Saldo < toleranciaFalta)
+            saldoTotal += dia.Saldo;
+        else if (dia.Saldo > toleranciaExtra) 
+            saldoTotal += dia.Saldo;
+    }
+
+    Console.WriteLine($"\n---\n{saldoTotal}");
+}
 
 
 Console.ReadKey();
