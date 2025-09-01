d209dea1aac9b135031d28b0075e190f1b74a812
Vitor Vicente <vitor.email2011@gmail.com>
2025-09-01 11:51:51 -0300

    Threads

---
+++ MeusExemplos/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine($"" +
+    $"--- CurrentThread info ---\n" +
+    $"Name..............: {Thread.CurrentThread.Name}\n" +
+    $"ThreadState.......: {Thread.CurrentThread.ThreadState}\n" +
+    $"ManagedThreadId...: {Thread.CurrentThread.ManagedThreadId}\n" +
+    $"Priority..........: {Thread.CurrentThread.Priority}\n" +
+    $"IsAlive...........: {Thread.CurrentThread.IsAlive}\n" +
+    $"IsBackground......: {Thread.CurrentThread.IsBackground}\n");
+
+var currThread = Thread.CurrentThread;
+
+currThread.Name = "t - 1";
+
+Console.WriteLine($"{currThread.Name} | {Thread.CurrentThread.Name}");
+
+//-----------------------------------------------------------------------------------------------------
+
+var novaThread = new Thread(() =>
+    {
+        Console.WriteLine($"Função execudata dentro da thread{Thread.CurrentThread.Name}");
+    });
+
+novaThread.Name = "nova thread";
+
+novaThread.Start();
---
+++ MeusExemplos/MultiThread/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+var cozinheiroVitor = new Thread(() =>
+{
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Colocou o arroz na panela");
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Ligou o fogão");
+    Thread.Sleep(5 * 1000);
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Arroz cozido!");
+
+})
+{
+    Name = "Cozinhar arroz"
+};
+
+var cozinheiroJoao = new Thread(() =>
+{
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Temperou a carne");
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Ligou o fogão");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Fritou o bife {i}");
+        Thread.Sleep(1 * 1000);
+    }
+    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Carne assada!");
+
+})
+{
+    Name = "Fritar a carne"
+};
+
+cozinheiroVitor.Start();
+cozinheiroJoao.Start();
