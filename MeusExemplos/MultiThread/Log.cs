82ac0873d4bc232bd793dad7fe9211ff56224c32
Vitor Vicente <vitor.email2011@gmail.com>
2025-09-05 18:58:30 -0300

    .

---
+++ MeusExemplos/MultiThread/Log.cs
@@ -0,0 +1,17 @@
+﻿namespace Tasks;
+
+public static class Log
+{
+    public static void Write(Guid guid, string name = "")
+    {
+        var t = Thread.CurrentThread;
+        Console.WriteLine($"" +
+            $"{DateTime.UtcNow};" +
+            $"{t.ManagedThreadId,-3};" +
+            $"{t.Name,-16};" +
+            $"{guid};" +
+            $"{name,-20};"
+            );
+
+    }
+}
--- MeusExemplos/MultiThread/Program.cs
+++ MeusExemplos/MultiThread/Program.cs
@@ -1,37 +1,54 @@
-﻿
+﻿using Tasks;
 
-var cozinheiroVitor = new Thread(() =>
-{
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Colocou o arroz na panela");
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Ligou o fogão");
-    Thread.Sleep(5 * 1000);
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Arroz cozido!");
 
-})
-{
-    Name = "Cozinhar arroz"
-};
+var id = Guid.NewGuid();
+Log.Write(id, "Principal inicio");
 
-var cozinheiroJoao = new Thread(() =>
-{
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Temperou a carne");
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Ligou o fogão");
-    for (int i = 0; i < 10; i++)
+
+
+var cozinheiroVitor =
+    new Thread(() =>
     {
-        Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Fritou o bife {i}");
-        Thread.Sleep(1 * 1000);
-    }
-    Console.WriteLine($"{DateTime.Now} {Thread.CurrentThread.Name} - Carne assada!");
+        var idT1 = Guid.NewGuid();
+        Log.Write(idT1, "Thread Cozinhar arroz - inicio");
 
-})
-{
-    Name = "Fritar a carne",
-    //IsBackground = true, // quando todas as tasks foreground terminam, as background são eliminadas
+        Thread.Sleep(5 * 1000);
 
-};
+        Log.Write(idT1, "Thread Cozinhar arroz - fim");
+    })
+    {
+        Name = "Cozinhar arroz"
+    };
 
-Console.WriteLine("Start nas threads");
+var cozinheiroJoao =
+    new Thread(() =>
+    {
+        var idT2 = Guid.NewGuid();
+        Log.Write(idT2, "Thread Fritar a carne - inicio");
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            Log.Write(idT2, "Fritou o bife {i}");
+            Thread.Sleep(1 * 1000);
+        }
+
+
+        Log.Write(idT2, "Thread Fritar a carne - fim");
+    })
+    {
+        Name = "Fritar a carne",
+        //IsBackground = true, // quando todas as tasks foreground terminam, as background são eliminadas
+
+    };
+
+Log.Write(id, "Start nas threads");
 cozinheiroVitor.Start();
 cozinheiroJoao.Start();
-Console.WriteLine("Startou as threads");
+Log.Write(id, "Startou as threads");
+
+
+
+Log.Write(id, "Principal fim");
+
 
