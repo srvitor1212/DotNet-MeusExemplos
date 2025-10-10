b38b24ac7769ebf396504165fffd4ca7983099ae
Vitor Vicente <vitor.v@ambevtech.com.br>
2025-10-10 12:32:32 -0300

    .

---
+++ MeusExemplos/Delegates/FuncExemplos.cs
@@ -0,0 +1,52 @@
+﻿namespace Delegates;
+public class FuncExemplos
+{
+    public static void Exemplo()
+    {
+        Func<string> func = RetValor;
+        Console.WriteLine($"Func sempre retorna algo: {func()}");
+
+
+        Func<int, string, string> funcDois = RetValor;
+        var retorno = funcDois(98, "reais");
+        Console.WriteLine($"Func sempre retorna algo: {retorno}");
+
+
+        var exemplo1 = ExecutarComportamento(DescontarPercentil, 100);
+        var exemplo2 = ExecutarComportamento(AplicarPercentil, 100);
+        Console.WriteLine($"exemplo1: {exemplo1} | exemplo2: {exemplo2} ");
+    }
+
+
+
+
+
+    private static string RetValor()
+    {
+        return "valor retorno";
+    }
+    private static string RetValor(int val, string str)
+    {
+        return $"valor retorno é {val} com {str}.";
+    }
+
+    private static decimal ExecutarComportamento(
+        Func<int, int, decimal> calculador,
+        int valor)
+    {
+        var result = calculador(valor, 7);
+
+        return result;
+    }
+
+    private static decimal DescontarPercentil(int valor, int percent)
+    {
+        decimal percentConvertido = (decimal)percent / 100;
+        return valor - (valor * percentConvertido);
+    }
+    private static decimal AplicarPercentil(int valor, int percent)
+    {
+        decimal percentConvertido = (decimal)percent / 100;
+        return valor + (valor * percentConvertido);
+    }
+}
--- MeusExemplos/Delegates/Program.cs
+++ MeusExemplos/Delegates/Program.cs
@@ -1,5 +1,20 @@
-﻿
+﻿using Delegates;
 
-using Delegates;
 
+/*
+Em C#, um delegate é um tipo que representa uma referência para um método.
+Ele funciona como um ponteiro de função seguro, permitindo armazenar, passar e chamar métodos de forma dinâmica.
+
+- porque usar delegates ao invés de passar o objeto como um parâmetro ?
+Passar um objeto como parâmetro → você passa dados.
+Passar um delegate como parâmetro → você passa comportamento (um método que será executado).
+ */
+
+// Action<T> - Representa um método que não retorna valor (void)
 ActionExemplos.Exemplo();
+
+
+// Func - Representa um método que retorna um valor
+FuncExemplos.Exemplo();
+
+// Predicate - Representa um método que recebe um parâmetro e retorna bool
